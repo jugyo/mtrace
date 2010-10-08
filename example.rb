b08944f13b224efffd1e63fdd435b9ffe7838025
jugyo <jugyo.org@gmail.com>
2010-10-08 23:46:38 +0900

    output link to open in TextMate

--- example.rb
+++ example.rb
@@ -1,3 +1,4 @@
+require 'mtrace'
 def fib(n)
   return n if n == 0 or n == 1
   fib(n-1) + fib(n-2) if n > 1
--- lib/mtrace.rb
+++ lib/mtrace.rb
@@ -2,7 +2,9 @@ mtrace_indent = 0
 set_trace_func lambda {|event, file, line, id, binding, klass|
   case event
   when 'call'
-    puts "\e[90m| " * mtrace_indent + "\e[35m#{klass}\e[90m#\e[36m#{id} \e[90m#{file}:#{line}\e[0m"
+    puts "\e[90m| " * mtrace_indent +
+      "\e[35m#{klass}\e[90m#\e[36m#{id} \e[90m#{file}:#{line}\e[0m" +
+      "#{'  ' * ([10 - mtrace_indent, 1].max)}\e[90mtxmt://open/?url=file://#{ File.expand_path(file).sub(File.expand_path('~'), '~') }&line=#{ line }\e[0m"
     mtrace_indent += 1
   when 'return'
     mtrace_indent -= 1
